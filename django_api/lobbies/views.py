bd2e41a333f55de06f72b62f23e3c15162783838
s41nteven <lolrhinox@gmail.com>
2022-08-09 06:27:28 +0200

    started to add view

--- django_api/lobbies/views.py
+++ django_api/lobbies/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Lobby
 
 # Create your views here.
+
+@api_view(['GET'])
+def getData(request):
+    lobby = Lobby.objects.all()
+    return Response(lobby)
