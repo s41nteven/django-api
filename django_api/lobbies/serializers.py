500436a8132f2123fb0801a464fbf2b42f46b5d9
s41nteven <lolrhinox@gmail.com>
2022-08-09 06:18:26 +0200

    setup on mac

--- django_api/lobbies/serializers.py
+++ django_api/lobbies/serializers.py
@@ -2,4 +2,6 @@ from rest_framework import serializers
 from .models import Lobby
 
 class LobbySerializer(serializers.ModelSerializer):
-	
+    class Meta:
+        model = Lobby
+        fields = ('id', 'name', 'description')
